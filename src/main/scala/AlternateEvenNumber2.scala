8b1e17e777e887b990eff56846b8d8448fee6c7e
farhan007000 <farhan007000@gmail.com>
2023-11-05 18:29:58 +0530

    Alternate Even number from 20 to 140 start from 22,26,30

---
+++ src/main/scala/AlternateEvenNumber2.scala
@@ -0,0 +1,12 @@
+object AlternateEvenNumber2 {
+  def main(args: Array[String]): Unit= {
+    println("Alternate Even number from 20 to 140 start from 22,26,30")
+    var num = 22
+
+    while (num <= 140){
+      println(num)
+      num+=4
+    }
+  }
+
+}
