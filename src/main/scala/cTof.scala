ecf4f83af858943152be1d6a87d81b9d2b4ef0ed
farhan007000 <farhan007000@gmail.com>
2023-11-05 15:27:13 +0530

    convert temp from C to F

---
+++ src/main/scala/cTof.scala
@@ -0,0 +1,11 @@
+object cTof {
+  def main(args: Array[String]): Unit = {
+    print("Enter the temperature in Celsius: ")
+    val tempCelsius = scala.io.StdIn.readDouble()
+    //F=(C * 9/5) + 32
+    val tempFahrenheit = (tempCelsius * 9/5 + 32)
+
+    println(s"$tempCelsius Celsius is equal to $tempFahrenheit Fahrenhait.")
+  }
+
+}
