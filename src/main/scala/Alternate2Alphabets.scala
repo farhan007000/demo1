bc256d4473e6146052ab47fb91365c9fb4dd86de
farhan007000 <farhan007000@gmail.com>
2023-11-06 17:22:42 +0530

    Alternate2Alphabets A and B

---
+++ src/main/scala/Alternate2Alphabets.scala
@@ -0,0 +1,13 @@
+object Alternate2Alphabets {
+  def main(args: Array[String]): Unit = {
+
+    for (i <- 1 to 100){
+      if(i%2 == 1){
+        print("A,")
+      }else{
+        print("B,")
+      }
+    }
+  }
+
+}
